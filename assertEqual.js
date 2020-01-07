b834ecfc4a259f080784892343611e7061461863
Ken Fyfe <kenfyfe@kenfyfe.com>
2020-01-07 00:30:38 +0000

    added templete literals/strings

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,11 @@
 const assertEqual = function(actual, expected) {
   let returnText;
   if (actual === expected) {
-    returnText = '✅ ' + 'Assertion Passed: ' + actual + ' === ' + expected;
+    // returnText = '✅ ' + 'Assertion Passed: ' + actual + ' === ' + expected;
+    returnText = `✅ Assertion Passed: ${actual} === ${expected}`;
   } else {
-    returnText = '🛑 ' + 'Assertion Failed: ' + actual + ' !== ' + expected;
+    // returnText = '🛑 ' + 'Assertion Failed: ' + actual + ' !== ' + expected;
+    returnText = `🛑 Assertion Failed: ${actual} !== ${expected}`;
   }
   console.log(returnText);
 };
