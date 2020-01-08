1499f1e7a4febe3e5e9290879b87d7cf6fef2b8c
Ken Fyfe <kenfyfe@kenfyfe.com>
2020-01-08 15:01:05 +0000

    updated with something

--- assertEqual.js
+++ assertEqual.js
@@ -2,10 +2,8 @@
 const assertEqual = function(actual, expected) {
   let returnText;
   if (actual === expected) {
-    // returnText = '✅ ' + 'Assertion Passed: ' + actual + ' === ' + expected;
     returnText = `✅ Assertion Passed: ${actual} === ${expected}`;
   } else {
-    // returnText = '🛑 ' + 'Assertion Failed: ' + actual + ' !== ' + expected;
     returnText = `🛑 Assertion Failed: ${actual} !== ${expected}`;
   }
   console.log(returnText);
