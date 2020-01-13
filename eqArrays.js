d5d6937fded4226bb9ddadbf7bfe4844f226a479
Ken Fyfe <kenfyfe@kenfyfe.com>
2020-01-13 20:52:02 +0000

    using include files now

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let returnText;
-  if (actual === expected) {
-    returnText = `✅ Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    returnText = `🛑 Assertion Failed: ${actual} !== ${expected}`;
-  }
-  console.log(returnText);
-};
-
 // compares the equivalance of two arrays
 const eqArrays = function(array1, array2) {
   if (array1.length === array2.length) {
@@ -22,11 +12,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-// test the functions!
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
-assertEqual(eqArrays([1, 2, 3, 1], [3, 2, 1]), false) // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+module.exports = eqArrays;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,10 @@
+// test the functions!
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays([1, 2, 3, 1], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
